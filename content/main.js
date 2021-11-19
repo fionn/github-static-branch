d0c27c715e4ca3ecf0854ad12d9b6c0c4326192a
Fionn Fitzmaurice <fionn@github.com>
2021-11-19 20:34:33 +0800

    Consider arbitrary default branches

--- content/main.js
+++ content/main.js
@@ -1,9 +1,11 @@
-elementIds = ["empty-setup-push-repo-echo", "empty-setup-new-repo-echo"]
+const elementIds = ["empty-setup-push-repo-echo", "empty-setup-new-repo-echo"]
 
 try {
+    const defaultBranch = document.getElementById(elementIds[0]).innerText.split("\n")[1].split(" -M ")[1]
+    console.log(browser.runtime.id + ": default branch is", defaultBranch)
     for(var i = 0; i < elementIds.length; i++) {
-        element = document.getElementById(elementIds[i])
-        element.innerHTML = element.innerHTML.replace("<span class=\"user-select-contain\">git branch -M main</span>\n", "").replace(" main", " $(git branch --show-current)")
+        let element = document.getElementById(elementIds[i])
+        element.innerHTML = element.innerHTML.replace("<span class=\"user-select-contain\">git branch -M " + defaultBranch + "</span>\n", "").replace(" " + defaultBranch, " $(git branch --show-current)")
         console.log(browser.runtime.id + ": replaced branch instructions for " + elementIds[i] + " ID")
     }
 } catch(error) {
